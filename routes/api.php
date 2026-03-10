fe92f195a9c620ed42defe53ef00daf26c4850e1
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2026-03-10 23:25:54 +0530

    add init

---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\BorrowController;
+use App\Http\Controllers\CupboardController;
+use App\Http\Controllers\PlaceController;
+
+
+ // Test Route
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'API working fine']);
+});
+
+//Authentication
+
+Route::post('/login', [AuthController::class, 'login']);
+
+
+//Protected Routes (Require Login)
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Items
+    Route::apiResource('items', ItemController::class);
+
+    // Cupboards
+    Route::apiResource('cupboards', CupboardController::class);
+
+    // Places
+    Route::apiResource('places', PlaceController::class);
+
+    // Borrow System
+    Route::post('/borrow', [BorrowController::class, 'borrow']);
+    Route::post('/return', [BorrowController::class, 'returnItem']);
+
+});
